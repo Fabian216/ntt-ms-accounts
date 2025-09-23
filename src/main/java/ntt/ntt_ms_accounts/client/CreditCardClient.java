fc44df720138fd29f2f121204cc42f8751aefb4b
Stephany Chiroque <chiroquerevolledo@gmail.com>
2025-09-23 17:27:38 -0500

    add circuit breaker

--- src/main/java/ntt/ntt_ms_accounts/client/CreditCardClient.java
+++ src/main/java/ntt/ntt_ms_accounts/client/CreditCardClient.java
@@ -3,8 +3,13 @@ package ntt.ntt_ms_accounts.client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.core.publisher.Mono;
 import ntt.ntt_ms_accounts.dto.CreditCardDto;
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 
 @Service
@@ -13,11 +18,19 @@ public class CreditCardClient {
 
     private final WebClient.Builder webClientBuilder;
 
+    @CircuitBreaker(name = "creditCards", fallbackMethod = "fallbackGetCard")
     public Mono<CreditCardDto> getCreditCardByCustomerId(String id) {
         return webClientBuilder.build()
                 .get().uri("lb://ntt-ms-api-gateway/creditCards/customer/{id}",id)
                 .retrieve()
                 .bodyToFlux(CreditCardDto.class)
-                .next();
+                .next()
+                .timeout(Duration.ofSeconds(2))
+                .onErrorResume(TimeoutException.class, ex -> fallbackGetCard(id, ex))
+                .onErrorResume(WebClientRequestException.class, ex -> fallbackGetCard(id, ex));
+    }
+
+    private Mono<CreditCardDto> fallbackGetCard(String id, Throwable ex) {
+        return Mono.error(new IllegalStateException("Servicio de tarjetas no disponible", ex));
     }
 }
--- src/main/java/ntt/ntt_ms_accounts/client/CustomerClient.java
+++ src/main/java/ntt/ntt_ms_accounts/client/CustomerClient.java
@@ -1,10 +1,14 @@
 package ntt.ntt_ms_accounts.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import ntt.ntt_ms_accounts.dto.CustomerResponseDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import java.util.concurrent.TimeoutException;
+import java.time.Duration;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 
 @Service
 @RequiredArgsConstructor
@@ -12,12 +16,19 @@ public class CustomerClient {
 
     private final WebClient.Builder webClientBuilder;
 
+    @CircuitBreaker(name = "customers", fallbackMethod = "fallbackGetCustomer")
     public Mono<CustomerResponseDto> getCustomerById(String id) {
         return webClientBuilder.build()
                 .get()
                 .uri("lb://ntt-ms-api-gateway/customers/{id}", id)
                 .retrieve()
-                .bodyToMono(CustomerResponseDto.class);
+                .bodyToMono(CustomerResponseDto.class)
+                .timeout(Duration.ofSeconds(2))
+                .onErrorResume(TimeoutException.class, ex -> fallbackGetCustomer(id, ex))
+                .onErrorResume(WebClientRequestException.class, ex -> fallbackGetCustomer(id, ex));
+    }
+    private Mono<CustomerResponseDto> fallbackGetCustomer(String id, Throwable ex) {
+        return Mono.error(new IllegalStateException("Servicio de clientes no disponible", ex));
     }
 
 }
